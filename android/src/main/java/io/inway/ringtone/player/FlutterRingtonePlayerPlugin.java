8bd8b62a786f9d9c188e61293d7fd60d8a06b42a
sander102907 <s.d.bruin@hotmail.com>
2020-10-25 00:05:28 +0200

    add write settings permission

--- android/src/main/java/io/inway/ringtone/player/FlutterRingtonePlayerPlugin.java
+++ android/src/main/java/io/inway/ringtone/player/FlutterRingtonePlayerPlugin.java
@@ -10,6 +10,7 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 import android.media.RingtoneManager;
+import android.media.Ringtone;
 import android.net.Uri;
 
 import java.util.Map;
