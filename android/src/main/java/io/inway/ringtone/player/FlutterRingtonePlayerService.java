596b29791aafd1c1884f1ad9c04234edac9b7f39
sander102907 <s.d.bruin@hotmail.com>
2020-10-24 21:39:57 +0200

    debugging custom sounds

--- android/src/main/java/io/inway/ringtone/player/FlutterRingtonePlayerService.java
+++ android/src/main/java/io/inway/ringtone/player/FlutterRingtonePlayerService.java
@@ -153,6 +153,7 @@ public class FlutterRingtonePlayerService extends Service {
             return path;
 
          } catch (Exception e) {
+            e.printStackTrace();
             switch (kind) {
                 case 1:
                     ringtoneType = RingtoneManager.TYPE_ALARM;
