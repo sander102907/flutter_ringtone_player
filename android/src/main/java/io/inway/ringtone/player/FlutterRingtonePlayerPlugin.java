85dd4aaaa56de365be28a0361bbc80a8097b38c8
sander102907 <s.d.bruin@hotmail.com>
2020-10-25 00:48:36 +0200

    add new get alarm sounds method

--- android/src/main/java/io/inway/ringtone/player/FlutterRingtonePlayerPlugin.java
+++ android/src/main/java/io/inway/ringtone/player/FlutterRingtonePlayerPlugin.java
@@ -141,6 +141,7 @@ public class FlutterRingtonePlayerPlugin implements MethodCallHandler {
                 String uriString = tonesCursor.getString(RingtoneManager.URI_COLUMN_INDEX);
                 Uri uri = Uri.parse(uriString + "/" + id);
                 String name = tonesCursor.getString(RingtoneManager.TITLE_COLUMN_INDEX);
+                System.out.println(name);
                 sounds.put(name, uri);
             } while (tonesCursor.moveToNext()); 
         }
