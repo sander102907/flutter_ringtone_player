aa093313ba47704eb57356bb504ec14627a0ce07
sander102907 <s.d.bruin@hotmail.com>
2021-02-21 16:27:35 +0100

    fix deprecation of setstreamtype as alarm to setaudioattributes

--- android/src/main/java/io/inway/ringtone/player/FlutterRingtonePlayerService.java
+++ android/src/main/java/io/inway/ringtone/player/FlutterRingtonePlayerService.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.media.AudioManager;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
+import android.media.AudioAttributes;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
